a947f48a511728bbed0daa7b9d36bc7252af0ef6
Icsperia <marianionut1112333@gmail.com>
2025-03-26 14:31:00 +0200

    Extindere baza de date

--- SQL/Test.sql
+++ SQL/Test.sql
@@ -1,7 +1,7 @@
 
 	USE art_gallery;
 	CREATE TABLE accounts (
-		id INT PRIMARY KEY AUTO_INCREMENT,
+		id_account INT PRIMARY KEY AUTO_INCREMENT,
 		first_name VARCHAR(255),
 		last_name VARCHAR(255),
 		email VARCHAR(255) UNIQUE,
@@ -16,7 +16,7 @@
 		art_name VARCHAR(255),
 		art_description TEXT,
 		price DECIMAL(10,2),
-		FOREIGN KEY (id_artist) REFERENCES artist(id_artist) ON DELETE CASCADE
+		FOREIGN KEY (id_artist) REFERENCES accounts(id_account) ON DELETE CASCADE
 	);
 
 	CREATE TABLE sales (
@@ -27,5 +27,26 @@
 		client_info TEXT,
 		FOREIGN KEY (id_art) REFERENCES art(id_art) ON DELETE CASCADE
 	);
+    
+    CREATE TABLE pencil_portraits(
+    id_pencil_portraits INT PRIMARY KEY AUTO_INCREMENT,
+    id_art INT,
+    sale_price DECIMAL(10,2),
+    FOREIGN KEY (id_art) REFERENCES art(id_art) ON DELETE CASCADE
+    );
 
-			
+	CREATE TABLE acrylic_paintings(
+    id_acrylic_portraits INT PRIMARY KEY AUTO_INCREMENT,
+    id_art INT,
+    sale_price DECIMAL(10,2),
+    FOREIGN KEY (id_art) REFERENCES art(id_art) ON DELETE CASCADE
+    );
+    
+    CREATE TABLE watercolor_paintings(
+    id_watercolor_paintings INT PRIMARY KEY AUTO_INCREMENT,
+    id_art INT,
+    sale_price DECIMAL(10,2),
+    FOREIGN KEY (id_art) REFERENCES art(id_art) ON DELETE CASCADE
+    );
+    
+
