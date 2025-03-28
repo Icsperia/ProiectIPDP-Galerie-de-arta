6d730edeb3de61d6b81da82fa1c8e66249e28f07
Icsperia <marianionut1112333@gmail.com>
2025-03-28 18:42:23 +0200

    Extindere baza de date

--- src/DatabaseConnection.js
+++ src/DatabaseConnection.js
@@ -49,40 +49,31 @@ app.post('/api/users', (req, res) => {
         res.status(201).send('User created successfully');
     });
 });
-//const mysql = require('mysql2');
 
-// Create a connection object
-/*const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'idfk',
-    database: 'art_gallery'
-});
+app.get('/users/:id', async (req, res) => {
+    try{
+        const {id_user} = req.params;
+        const data = await connection.promise().query(
+            'SELECT * FROM user WHERE id_user = ?;',
+        )
 
-// Connect to the database
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err.stack);
-        return;
+    }catch(err){
+        res.status(500).json({error: err.stack});
     }
-    console.log('Connected to the database as ID:', connection.threadId);
+})
 
-    // Insert a new row into the table
- ;
 
-    // Close the connection when done
-    const selectQuery = 'SELECT * FROM artist';
-    connection.query(selectQuery, (err, rows) => {
-        if (err) {
-            console.error('Error fetching data:', err.stack);
-            return;
-        }
 
-        // Log the fetched data to the console
-        console.log('All rows from artworks table:', rows);
-    });
-
-    // Close the connection when done
-    connection.end();
-});*/
-
+app.patch('/api/users/:id', async (req, res) => {
+    try{
+     const {id_user} = req.params;
+     const {email, password, first_name, last_name, user_name} = req.body;
+     const update = await connection.promise().query(
+         'UPDATE user SET email = ?, password = ?, first_name =?, last_name =?, user_name = ? WHERE id_user = ?;',
+         [email, password, first_name, last_name, user_name]
+     );
+     res.status(200).json({message: 'User updated successfully'});
+    }catch(err){
+        res.status(500).json({error: err.stack});
+    }
+})
