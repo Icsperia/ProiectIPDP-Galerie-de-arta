8553875cb49a8324ecb5e256df755f54b52e2400
Icsperia <marianionut1112333@gmail.com>
2025-04-04 09:02:24 +0300

    Join-urile

--- src/DatabaseConnection.js
+++ src/DatabaseConnection.js
@@ -1,7 +1,10 @@
+import express from 'express';
+import mysql from 'mysql2';
 
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
+import bodyParser from 'body-parser';
+//const express = require('express');
+//const mysql = require('mysql12');
+//const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,11 +12,15 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 // MySQL Connection
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: '192.168.10.130',
     user: 'root',
-    password: 'idfk',
+    password: '1234',
     database: 'art_gallery'
 });
+
+
+
+
 // Connect to MySQL
 db.connect((err) => {
     if (err) {
@@ -22,6 +29,8 @@ db.connect((err) => {
     }
     console.log('Connected to MySQL as ID ' + db.threadId);
 });
+
+
 // Routes
 app.get('/api/users', (req, res) => {
     db.query('SELECT * FROM user', (err, results) => {
@@ -68,3 +77,112 @@ app.patch('/api/users/:id', async (req, res) => {
         res.status(500).json({error: err.stack});
     }
 })
+
+app.delete('/api/users/:id', async (req, res) => {
+    const {id_user} = req.params.id;
+    const   query = `DELETE FROM user WHERE id_user = ?;`;
+    db.query(query,[id_user],(err) => {
+        if (err) {
+        res.status(500).json({error: err.message});
+    }else {
+            res.status(200).json({message: 'User deleted successfully'});
+        }
+        })
+})
+
+/*const query = `ALTER TABLE  random_drawings ADD COLUMN location VARCHAR(200)`;
+
+db.query(query, (err, results) => {  // Ensure the query string is used properly
+    if (err) {
+        console.error('Error executing query: ' + err.stack);
+    } else {
+        console.log('Query executed successfully', results);
+    }})*/
+
+const query = 'Select u.id_user, u.first_name, u.last_name, u.user_name, dc.id_date_comanda, dc.status, dc.adresa, dc.data ' +
+    'from user u JOIN date_comanda dc ON u.id_user = dc.id_user' ;
+db.query(query, (err, results) => {
+    if(err){
+        console.log ('Error executing query: ' + err.stack);
+    }else{
+        console.log('Query executed successfully', results);
+    }
+})
+
+const query1 = 'SELECT ar.id_artist, u.user_name, a.id_art, a.pret, a.descriere ' +
+    'FROM artist ar ' +
+    'INNER JOIN user u ON ar.id_user = u.id_user ' +
+    'INNER JOIN art a ON ar.id_artist = a.id_artist';
+
+db.query(query1, (err, results) => {
+    if(err){
+        console.error('Error executing query: ' + err.stack);
+    }else{
+        console.log('Query executed successfully', results);
+    }
+});
+
+const query3 = 'Select u.id_user, u.user_name, a.id_art, a.pret, a.descriere, dc.status, dc.adresa, dc.data' +
+                      ' from user u JOIN date_comanda dc ON u.id_user = dc.id_user' +
+                      ' JOIN art a ON dc.id_art = a.id_art' ;
+
+db.query(query3, (err, results) => {
+    if(err){
+        console.error('Error executing query: ' + err.stack);
+    }else{
+        console.log('Query executed successfully', results);
+    }
+});
+
+const query4 = `
+    SELECT a.id_art, a.pret AS pret_general, 
+           pp.pret AS pret_pencil, 
+           ap.pret AS pret_acrylic, 
+           wp.pret AS pret_watercolor, 
+           d.pret AS pret_drawing, 
+           rd.pret AS pret_random 
+    FROM art a 
+    LEFT JOIN pencil_portraits pp ON a.id_art = pp.id_art 
+    LEFT JOIN acrylic_paintings ap ON a.id_art = ap.id_art 
+    LEFT JOIN watercolor_paintings wp ON a.id_art = wp.id_art 
+    LEFT JOIN drawings d ON a.id_art = d.id_art 
+    LEFT JOIN random_drawings rd ON a.id_art = rd.id_art
+`;
+
+db.query(query4, (err, results) => {
+    if(err){
+        console.error('Error executing query: ' + err.stack);
+    }else{
+        console.log('Query executed successfully', results);
+    }
+});
+
+const query5 = `
+    SELECT uc.id_user_comanda, u.user_name, dc.id_date_comanda, dc.status, dc.adresa, dc.data 
+    FROM user_comanda uc
+    JOIN user u ON uc.id_user = u.id_user 
+    JOIN date_comanda dc ON uc.id_date_comanda = dc.id_date_comanda
+`;
+
+db.query(query5, (err, results) => {
+    if(err){
+        console.error('Error executing query: ' + err.stack);
+    }else{
+        console.log('Query executed successfully', results);
+    }
+});
+
+const query6 = `SELECT dc.id_date_comanda, u.user_name, a.id_art, a.descriere, a.pret, dc.status, dc.adresa, dc.data
+FROM date_comanda dc
+JOIN user u ON dc.id_user = u.id_user
+JOIN art a ON dc.id_art = a.id_art
+
+`;
+
+db.query(query6, (err, results) => {
+    if(err){
+        console.error('Error executing query: ' + err.stack);
+    }else{
+        console.log('Query executed successfully', results);
+    }
+});
