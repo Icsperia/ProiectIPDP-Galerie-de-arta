7a8f80ae53e9b0941c19a938a47b44ed12b20d37
Icsperia <marianionut1112333@gmail.com>
2025-05-07 15:48:12 +0300

    backend care ar trebui sa mearga

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,23 +1,29 @@
-// middleware/users.js
-
-const jwt = require("jsonwebtoken");
-
 module.exports = {
     validateRegister: (req, res, next) => {
-        // username min length 3
-        if (!req.body.username || req.body.username.length < 3) {
-            return res.status(400).send({
-                message: 'Please enter a username with min. 3 chars',
-            });
+        let errors = {};
+
+
+        if (!req.body.user_name || req.body.user_name.length < 3) {
+            errors.unError = 'Username must be at least 3 characters long';
         }
+
         // password min 6 chars
         if (!req.body.password || req.body.password.length < 6) {
-            return res.status(400).send({
-                message: 'Please enter a password with min. 6 chars',
+            errors.passError = 'Password must be at least 6 characters long';
+        }
+
+
+
+        // If there are any errors, render the registration page with the error messages
+        if (Object.keys(errors).length > 0) {
+            return res.render('register', {
+                unError: errors.unError,
+                passError: errors.passError,
+                passMatchError: errors.passMatchError
             });
         }
-        // password (repeat) must match
 
+        // If all validations pass, continue to the next middleware
         next();
     }
 };
