59bfe973d558471d3e430fb15c9555044ce54732
Icsperia <marianionut1112333@gmail.com>
2025-05-13 16:15:34 +0300

    ceva

--- DatabaseConnection.js
+++ DatabaseConnection.js
@@ -1,16 +1,12 @@
 const mysql = require('mysql2');
 
-const db = mysql.createPool({
-    host: 'localhost',
+const db = mysql.createConnection({
+    host: '192.168.10.130',
     user: 'root',
     password: '1234',
-    database: 'art_gallery',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    database: 'art_gallery'
 });
 
-/*
 db.connect((err) => {
     if (err) {
         console.error('Error connecting to MySQL:', err.stack);
@@ -18,10 +14,9 @@ db.connect((err) => {
     }
     console.log('Connected to MySQL as ID', db.threadId);
 });
-
 const net = require('net');
 net.createConnection(3306, '192.168.10.130')
     .on('connect', () => console.log('MySQL port open'))
     .on('error', () => console.error('Cannot connect to MySQL server'));
-*/
+
 module.exports = db;
